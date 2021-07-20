c57b9de8a45127e6df6e36bf2a699c334b39ca75
Juan Bolaños <58042023+juano999@users.noreply.github.com>
2021-07-19 20:07:41 -0500

    Add files via upload

---
+++ Scripttarea.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import json
+from argparse import ArgumentParser
+import requests
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from bson import json_util, ObjectId
+import couchdb
+
+
+# In[4]:
+
+
+
+URL = 'http://admin:admin@localhost:5984'
+print(URL)
+
+try:
+    response = requests.get(URL)
+    if response.status_code == 200:
+        print('CouchDB connection: Success')
+    if response.status_code == 401:
+        print('CouchDB connection: failed', response.json())
+except requests.ConnectionError as e:
+    raise e
+print("hola")  
+server=couchdb.Server(URL)
+HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+CLIENT=MongoClient('mongodb+srv://usuario1:usuario1@cluster0.snkvt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+try:
+    CLIENT.admin.command('ismaster')
+    print('MongoDB connection: Success')
+except ConnectionFailure as cf:
+    print('MongoDB connection: failed', cf)
+
+
+DBS=['TareaAnalisis']
+
+print("hola")  
+try:
+    dbc=server.create('mongo_destino2')
+except:
+    dbc=server['mongo_destino2']
+ 
+
+for db in DBS:
+    if db not in ('admin', 'local','config'):  
+        cols = CLIENT[db].list_collection_names()  
+        for col in cols:
+            print('Querying documents from collection {} in database {}'.format(col, db))
+            for x in CLIENT[db][col].find():  
+                try:
+                    
+                    documents=json.loads(json_util.dumps(x))
+
+                    documents["_id"]=str(documents["_id"]["$oid"])
+
+
+                    print(documents)
+                    doc=dbc.save(documents)
+
+                except TypeError as t:
+
+                    print('current document raised error: {}'.format(t))
+                    SKIPPED.append(x)  # creating list of skipped documents for later analysis
+                    continue    # continue to next document
+                except Exception as e:
+                    raise e
+
+
+# In[10]:
+
+
+
+
+
+# In[19]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
